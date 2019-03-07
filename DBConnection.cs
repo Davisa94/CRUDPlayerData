85696f4787d08afb9fa3ebddad8b7697ef7e3484
davisa94 <austin.i.davis94@gmail.com>
2019-03-07 15:07:58 -0700

    Changed db connection to prepared statments

--- DBConnection.cs
+++ DBConnection.cs
@@ -9,33 +9,48 @@ namespace WindowsFormsApp1
 {
     class DBConnection
     {
-        public bool SavePlayer(Player player)
+        public int SavePlayer(Player player)
         {
-            //Create connection string for local DB
-            string connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            //Use connection string to make a connection to the DB
-            SqlConnection connection = new SqlConnection(connectString);
-            //Open the connection
-            connection.Open();
+            int numRecords = -1;
             //Check if th emiddlename needs to be nulled
             string middleName = player.MiddleName;
-            if(middleName == "") { middleName = "NULL"; }
-            //Create the insertion query
-            //TODO: is there a way to prepare this statment
-            string query = "INSERT INTO PlayerTeamData.dbo.playerInfo (FirstName, MiddleName, LastName, DateOfBirth, HeightInches, JerseyNumber) " +
-                " VALUES (@firstName,@middleName+, @lastName, @DOB, @height, @jerseyNum)";
-            //Prepare the query
-            using (SqlCommand querySavePlayer = new SqlCommand(query))
+            if (middleName == "") { middleName = "NULL"; }
+            //Create connection string for local DB
+            string connectString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection connection = new SqlConnection(connectString))
             {
-                querySavePlayer.Connection = connection;
-            }
-                //Create the command using the query and the connection
-                SqlCommand command = new SqlCommand(query, connection);
-            //execute the command
-            command.ExecuteNonQuery();
+                //Create the insertion query
+                //TODO: is there a way to prepare this statment
+                string query = "INSERT INTO PlayerTeamData.dbo.playerInfo (FirstName, MiddleName, LastName, DateOfBirth, HeightInches, JerseyNumber) " +
+                    " VALUES (@firstName,@middleName+, @lastName, @DOB, @height, @jerseyNum)";
+                //Prepare the query
+                using (SqlCommand command = new SqlCommand(query))
+                {
+                    command.Connection = connection;
+                    //Set the command type
+                    command.CommandType = System.Data.CommandType.Text;
+                    //Set the command text equal to the query:
+                    command.CommandText = query;
+                    //Prepare the variables:
+                    command.Parameters.AddWithValue("@firstName", player.FirstName);
+                    command.Parameters.AddWithValue("@middleName", player.MiddleName);
+                    command.Parameters.AddWithValue("@lastName", player.LastName);
+                    command.Parameters.AddWithValue("@DOB", player.DateOfBirth);
+                    command.Parameters.AddWithValue("@height", player.HeightInches);
+                    command.Parameters.AddWithValue("@jerseyNum", player.JerseyNum);
 
-            connection.Close();
-            return true;
+                    //TODO: add try catch:
+                    connection.Open();
+                    numRecords = command.ExecuteNonQuery();
+                    connection.Close();
+
+
+                }
+            }
+                
+
+            
+            return numRecords;
         }
     }
 }
